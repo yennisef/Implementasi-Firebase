59bc9f1271cb1f28815de924515cddf4af8d8aa1
yennisef <yongrhee1998@gmail.com>
2021-10-04 00:08:44 +0700

    initial commit

---
+++ Implementasi-Firebase-firebase/Assets/Scripts/DebuggerOnScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebuggerOnScene : MonoBehaviour
+{
+    public Text debuggerText;
+    string myLog = "*begin log";
+    string filename = "";
+    bool doShow = true;
+    int kChars = 700;
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void OnEnable() { Application.logMessageReceived += Log; }
+    void OnDisable() { Application.logMessageReceived -= Log; }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { doShow = !doShow; }
+        if (debuggerText != null)
+            debuggerText.text = myLog;
+    }
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        // for onscreen...
+        myLog = myLog + "\n" + logString;
+        if (myLog.Length > kChars) { myLog = myLog.Substring(myLog.Length - kChars); }
+
+        // for the file ...
+        if (filename == "")
+        {
+            string d = System.Environment.GetFolderPath(
+               System.Environment.SpecialFolder.Desktop) + "/YOUR_LOGS";
+            System.IO.Directory.CreateDirectory(d);
+            string r = Random.Range(1000, 9999).ToString();
+            filename = d + "/log-" + r + ".txt";
+        }
+        try { System.IO.File.AppendAllText(filename, logString + "\n"); }
+        catch { }
+    }
+
+    void OnGUI()
+    {
+        if (!doShow) { return; }
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity,
+           new Vector3(Screen.width / 1200.0f, Screen.height / 800.0f, 1.0f));
+        GUI.TextArea(new Rect(10, 10, 540, 370), myLog);
+    }
+}
---
+++ Implementasi-Firebase-firebase/README.md
@@ -0,0 +1,3 @@
+# Implementasi-Firebase
+## Assignment Chapter 8 Game Progrogramming Studi Independen Agate
+Cloud Save/Load and Firebase Analytics
